54f021fd7c366836765447fe0bc0794359da3ccb
UJJWAL JOSHI <103943864+Ujjwal1110@users.noreply.github.com>
2022-08-25 23:53:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+         
+         <form id="form">
+        <h1>Log In</h1>
+        <input placeholder="Enter 10-Digit Mobile Number" id="mobile" type="Number"/>
+        {/* <hr style="width:85%"/> */}
+        <input placeholder="Enter your password" id="password" type="password" />
+        {/* <hr style="width:85%"/> */}
+        <input type="email" id="email" placeholder="Enter your Email" />
+        {/* <hr style="width:85%"/> */}
+       <input type="submit" id="Cont" value="LogIn" />
+       <div><p>New User? <a id="anchor"href="signup.html"><span>Sign Up</span></a> </p></div>
+    </form> 
+
+    </div>
+  );
+}
+
+export default App;
