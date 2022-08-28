a123f9c4c3b801392bf20b6864996c5b6d446750
UJJWAL JOSHI <103943864+Ujjwal1110@users.noreply.github.com>
2022-08-28 19:11:06 +0530

    done

--- New folder (4)/src/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import react from "react"
-export default function Navbar(){
-    return (
-        <>
-            <Link to="/login">Login</Link>
-        </>
-    )
-}
