4843232a5504b5806724a2be09b81ea88cea1dac
UJJWAL JOSHI <103943864+Ujjwal1110@users.noreply.github.com>
2022-08-28 19:12:18 +0530

    Final Push

---
+++ src/Allroute/Route.jsx
@@ -0,0 +1,12 @@
+// import Login from "../Login";
+// import {Routes, Route} from "react-router-dom";
+// import {  BrowserRouter } from "react-router-dom"
+// const Allroute = () => {
+//     return (
+//         <BrowserRouter>
+//         <Routes>
+//             <Route path="/login" element={<Login />} />
+//         </Routes>
+//         </BrowserRouter>
+//     )
+// }
---
+++ src/Login.jsx
@@ -0,0 +1,23 @@
+
+
+
+const Login =  () => {
+return (
+    <div>
+    <form id="form">
+    <h1>Log In</h1>
+    <input placeholder="Enter 10-Digit Mobile Number" id="mobile" type="Number"/>
+    {/* <hr style="width:85%"/> */}
+    <input placeholder="Enter your password" id="password" type="password" />
+    {/* <hr style="width:85%"/> */}
+    <input type="email" id="email" placeholder="Enter your Email" />
+    {/* <hr style="width:85%"/> */}
+   <input type="submit" id="Cont" value="LogIn" />
+   <div><p>New User? <a id="anchor"href="signup.html"><span>Sign Up</span></a> </p></div>
+   </form>
+    </div>
+    
+)
+}
+
+export default Login
